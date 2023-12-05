4285d64f948e70ba945eaec348fa09ca166858ba
hastrotech1 <hastrotech1@gmail.com>
2023-12-05 16:08:51 +0100

    updated

--- shell.h
+++ shell.h
@@ -1,10 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+/**
+ * Header guards for shell
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <sys/syscall.h>
+#include <err.h>
+
+/**
+ * Function prototypes
+ */
 
 void disprompt(void);
 void printer(const char *msg);
