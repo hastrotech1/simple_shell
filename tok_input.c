6d85dabd626e5391221cd84681b7e89ce2d5e144
hastrotech1 <hastrotech1@gmail.com>
2023-12-05 18:38:38 +0100

    tok_input

---
+++ tok_input.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * tok_input - This function tokinnize the command to separete the
+ * command and its arguments using the strtok function.
+ *
+ * @_str: The input string to be tokinized.
+ *
+ * Return: return token  as output when successful.
+ */
+
+char **tok_input(char *_str)
+{
+	char *tok = strtok(_str, dli);
+	char *token = NULL;
+	char *deli = "\n\t";
+	size_t tok_count = 0;
+
+	while (tok != NULL)
+	{
+		token = realloc(token, sizeof(char *) * (tok_count + 1));
+		if (token == NULL)
+		{
+			perror("Allocation Failed");
+			exit(EXIT_FAILURE);
+		}
+		token[tok_count++] = strdup(tok);
+		if (token[tok_count - 1] == NULL)
+		{
+			perror("Token Failed");
+			exit(EXIT_FAILURE);
+		}
+		tok = strtok(NULL, deli);
+	}
+	token = realloc(token, sizeof(char *) * (tok_count + 1));
+	if (token == NULL)
+	{
+		perror("Failed Memory Allocation");
+		exit(EXIT_FAILURE);
+	}
+	token[tok_count] = NULL;
+	free(token);
+	return (token);
+}
