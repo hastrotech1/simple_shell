ba49833c24e6dd74b8070991e54434a2386809f1
ELKINDMAN <elkanahkindness@gmail.com>
2023-12-08 19:17:30 +0100

    test.c: ignore this file

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int a = 32, int b = 8, int c;
+
+	printf("Enter any value: ");
+	scanf("%d", &c);
+
+	int result = a*b*c;
+	printf("Your final value is %d\n", result);
+
+return (0);
+}
